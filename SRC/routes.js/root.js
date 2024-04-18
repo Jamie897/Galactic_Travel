56a9920237a1c45a8348e2bfb33a98d17534f2ab
Jamie897 <jamiedanielle456@gmail.com>
2024-04-18 10:33:29 -0500

    .

--- SRC/routes.js/root.js
+++ SRC/routes.js/root.js
@@ -6,7 +6,7 @@ export default function Root() {
   return (
     <>
       <div id="sidebar">
-        <h1>Space Travelers&apos; Hub</h1>
+        <h1>Galactic Travel&apos; </h1>
         <nav>
           <ul>
             <li>
